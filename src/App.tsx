f17023b06e61566c5dd9e98880e0c3bfe9142641
xhojamusicagency-cmd <xhojamusicagency@gmail.com>
2026-02-08 17:03:54 -0500

    Fix missing Navigate import in App.tsx to fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import Layout from './components/Layout';
 import Home from './pages/Home';
@@ -38,4 +38,5 @@ function App() {
 }
 
 export default App;
+
 
